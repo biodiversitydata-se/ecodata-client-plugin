f5be5a6c5c8a1f69d69beb67f5d97c9c2af91017
chrisala <chris.godwin.ala@gmail.com>
2018-06-26 09:32:43 +1000

    Fixing row formatting in edit mode.

--- grails-app/taglib/au/org/ala/ecodata/forms/ModelTagLib.groovy
+++ grails-app/taglib/au/org/ala/ecodata/forms/ModelTagLib.groovy
@@ -470,6 +470,9 @@ class ModelTagLib {
         } else {
             at.addSpan("span${layoutContext.span}")
             out << "<span${at.toString()}>"
+            if (model.type != "number" && attrs.edit) {
+                elementAttributes.addClass 'span12'
+            }
         }
 
         out << INDENT << dataTag(attrs, model, layoutContext.dataContext, attrs.edit, elementAttributes, null, labelAttributes)
