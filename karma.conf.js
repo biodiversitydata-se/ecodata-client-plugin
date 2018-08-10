ca7a132692ebc2796f5c24c3c6cbe7e5eee73c66
chrisala <chris.godwin.ala@gmail.com>
2018-08-10 19:38:19 +1000

    Added test coverage to karma configuration

--- karma.conf.js
+++ karma.conf.js
@@ -46,13 +46,25 @@ module.exports = function (config) {
 
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-        preprocessors: {},
+        preprocessors: {
+            'grails-app/assets/javascripts/*.js':['coverage']
+        },
 
 
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
         reporters: ['progress', 'coverage'],
+
+        coverageReporter: {
+            'dir':'./target',
+            'type':"text",
+            check: {
+                global: {
+                    lines: 25
+                }
+            }
+        },
 
         // web server port
         port: 9876,
