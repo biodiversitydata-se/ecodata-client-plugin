98ba78cdee66dc1774687ca3e9cc1aee2b423dc4
temi <temi.varghese@csiro.au>
2018-09-04 15:34:32 +1000

    adjusting grails version for Biocollect

--- EcodataClientPluginGrailsPlugin.groovy
+++ EcodataClientPluginGrailsPlugin.groovy
@@ -2,7 +2,7 @@ class EcodataClientPluginGrailsPlugin {
     // the plugin version
     def version = "1.0"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.5 > *"
+    def grailsVersion = "2.4 > *"
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp"
