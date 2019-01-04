b96d0f03b458240fdaf9580c7ff3e77ca381f985
Chris <chris.godwin.ala@gmail.com>
2019-01-04 11:07:37 +1100

    Added turf simplify. #1736

--- turf-package-instructions.js
+++ turf-package-instructions.js
@@ -5,5 +5,6 @@ module.exports = {
     area: require('@turf/area').default,
     length: require('@turf/length').default,
     bbox: require('@turf/bbox').default,
-    convex: require('@turf/convex').default
+    convex: require('@turf/convex').default,
+    simplify: require('@turf/simplify').default
 };
