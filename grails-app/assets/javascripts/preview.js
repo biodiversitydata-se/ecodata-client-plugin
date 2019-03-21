feea6d3fa5785e290827d924ad4a8170e468295c
Chris <chris.godwin.ala@gmail.com>
2019-03-22 10:41:06 +1100

    Updated preview page to use newer file upload #19

--- grails-app/assets/javascripts/preview.js
+++ grails-app/assets/javascripts/preview.js
@@ -4,10 +4,7 @@
 //= require bootstrap/js/bootstrap.js
 //= require bootstrap-datepicker/js/bootstrap-datepicker.js
 //= require modal-fullscreen-bootstrap-2.3.2/js/bootstrap-modal-fullscreen.js
-//= require jquery-ui/jquery-ui-1.9.2.custom.js
-//= require fileupload-9.0.0/jquery.fileupload.js
-//= require fileupload-9.0.0/jquery.fileupload-ui.js
-//= require fileupload-9.0.0/tmpl.js
+//= require file-upload-with-tmpl-manifest.js
 //= require underscorejs/1.8.3/underscore.js
 //= require fancybox/jquery.fancybox
 //= require select2/4.0.3/js/select2.full
