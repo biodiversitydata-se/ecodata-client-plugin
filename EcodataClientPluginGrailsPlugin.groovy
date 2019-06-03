01d966cc90e80ce271031f4c256a7d5bbd910dde
chrisala <chrisala@users.noreply.github.com>
2019-06-03 12:26:35 +1000

    v1.7

--- EcodataClientPluginGrailsPlugin.groovy
+++ EcodataClientPluginGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class EcodataClientPluginGrailsPlugin {
     // the plugin version
-    def version = "1.7-SNAPSHOT"
+    def version = "1.7"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.4 > *"
     // resources that are excluded from plugin packaging
