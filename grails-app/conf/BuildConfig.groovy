c3da189ecef99b0ebda883f92d1acc4d53cb4248
Chris <chris.godwin.ala@gmail.com>
2019-09-16 08:07:06 +1000

    Reverted phantomjs driver version.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -73,7 +73,7 @@ grails.project.dependency.resolution = {
         test "org.seleniumhq.selenium:selenium-support:2.53.1"
         test "org.seleniumhq.selenium:selenium-firefox-driver:2.53.1"
         test "org.seleniumhq.selenium:selenium-chrome-driver:2.53.1"
-        test "com.codeborne:phantomjsdriver:1.4.4"
+        test "com.codeborne:phantomjsdriver:1.2.1"
         test "net.sourceforge.nekohtml:nekohtml:1.9.22"
     }
 
