3a3afb6252b2b71c8aa08fd21aec570bf3551ab9
temi <temi.varghese@csiro.au>
2020-01-09 16:56:00 +1100

    added instant template compile to pick changes during development

--- grails-app/init/ecodata/client/plugin/BootStrap.groovy
+++ grails-app/init/ecodata/client/plugin/BootStrap.groovy
@@ -1,8 +1,14 @@
 package ecodata.client.plugin
 
+import asset.pipeline.AssetPipelineConfigHolder
+import au.org.ala.ecodata.forms.TemplateFileAssetResolver
+
 class BootStrap {
 
     def init = { servletContext ->
+        String pluginPath = '.'
+        def templateFileAssetResolver = new TemplateFileAssetResolver('templates', "${pluginPath}/grails-app/assets/components", false, '/compile/ecodata-templates.js', '/template')
+        AssetPipelineConfigHolder.resolvers.add(0, templateFileAssetResolver)
     }
     def destroy = {
     }
