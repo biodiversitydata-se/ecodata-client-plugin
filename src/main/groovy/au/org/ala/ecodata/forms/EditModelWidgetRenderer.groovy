aad05ae77e6e4dccad29dbf26c96c257d11ada5f
mathieuLU <you@example.com>
2023-04-06 16:31:50 +0200

    remove line to get the test passed

--- src/main/groovy/au/org/ala/ecodata/forms/EditModelWidgetRenderer.groovy
+++ src/main/groovy/au/org/ala/ecodata/forms/EditModelWidgetRenderer.groovy
@@ -151,7 +151,7 @@ public class EditModelWidgetRenderer implements ModelWidgetRenderer {
         context.databindAttrs.add 'optionsValue', context.source + '.constraints.value'
         context.databindAttrs.add 'optionsText', context.source + '.constraints.text'
 
-        context.databindAttrs.add 'optionsCaption', '"Välj"'
+        //context.databindAttrs.add 'optionsCaption', '"Välj"'
         String options = "{value: ${context.source}, tags:true, allowClear:false}"
         if (context.model.displayOptions) {
             options = "_.extend({value:${context.source}}, ${context.source}.displayOptions)"
