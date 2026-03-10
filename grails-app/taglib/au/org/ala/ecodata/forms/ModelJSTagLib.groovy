ee8d6f961622994c1c67db9d65a46c4137ecb339
Mathieu Blanchet <mathieu.blanchet@biol.lu.se>
2026-03-10 14:05:56 +0100

    hide first row in table for forms

--- grails-app/taglib/au/org/ala/ecodata/forms/ModelJSTagLib.groovy
+++ grails-app/taglib/au/org/ala/ecodata/forms/ModelJSTagLib.groovy
@@ -750,8 +750,10 @@ class ModelJSTagLib {
 
         // If there are no default rows, insert a single blank row and make it available for editing.
         if (attrs.edit && model.defaultRows == null) {
-            // modif LU : don't add empty rows as default => comment addRow line below
-            // insertDefaultModel = "${ctx.propertyPath}.${model.name}.addRow();"
+            // modif LU : don't add empty rows as default for observations fields
+            if (!model.name?.toLowerCase()?.contains("observations")) {
+                insertDefaultModel = "${ctx.propertyPath}.${model.name}.addRow();"
+            }
         }
 
         out << """var context = _.extend({}, context, {parent:self, listName:'${model.name}'});"""
